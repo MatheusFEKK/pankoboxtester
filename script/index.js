1b603bc06d6248dc1f5cf9f288003d073d55011c
Matheus Felipe <46269118+MatheusFEKK@users.noreply.github.com>
2024-07-08 17:02:45 -0300

    Atualização com o repositório privado

--- script/index.js
+++ script/index.js
@@ -1,15 +1,16 @@
 let karaagebtn = document.getElementById('karaagebuy');
 let body = document.body;
+let backgroundpage = document.getElementById('background_page');
 
 function karaage_receipt(){
-    let karaagereceipt = "<div class='receipt-background'><button class='button-close' onclick='receipt_close()'>X</button><h2>Karaage</h2><div class='meal-product-details'><img class='receipt-img' src='img/receipt/karaage_receita.png' style='height: 37.5rem; width: 37.5rem;'><div class='receipt-description'><aside><h3>Receita</h3><h4>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores illo consequuntur qui. Assumenda, ducimus harum in mollitia voluptates nisi blanditiis et laborum autem, magni qui vitae eveniet molestias soluta amet.</h4></aside><div id='button'></div></div></div><div class='receipt-buy-button'><a class='receipt-buy inert' href='login.html'>Peça já</a></div></div>";
+    let karaagereceipt = "<div class='receipt-background'><button class='button-close' onclick='receipt_close()'>X</button><h2>Karaage</h2><div class='meal-product-details'><img class='receipt-img' src='img/receipt/karaage_receita.png' style='height: 37.5rem; width: 37.5rem;'><div class='receipt-description'><aside><h3 style='font-weight:900;'>Ingredientes</h3><h4><ul class='receipt-ingredient'><li>900g de coxas de frango sem pele e com osso, cortadas em pedaços pequenos.</li><li>⅓ Xícara de molho de soja.</li><li>2 colheres de sopa de saquê.</li><li>25g de gengibre fresco ralado</li><li>170g de amido de batata (katakuriko)</li><li>Óleo vegetal para fritar</li><li>½ limão (para servir)</li></ul></h4></aside><div id='button'></div></div></div><div class='receipt-buy-button'><a class='receipt-buy inert' href='login.html'>Peça já</a></div></div>";
 
     let karaagemenu = document.createElement("div");
     karaagemenu.innerHTML =  karaagereceipt;
 
     document.body.appendChild(karaagemenu);
     document.body.style.overflow = "hidden";
-
+    backgroundpage.style.filter = "blur(10px)";
 
 }
 
@@ -22,7 +23,18 @@ function receipt_close(){
     }
 
     document.body.style.overflow = "auto";
+    backgroundpage.style.filter = "none";
 
 
 
+}
+
+function dropdownmenu(){
+
+    let divmenu = document.getElementById('menudrop');
+    let buttondropdown = document.getElementById('dropmenu');
+    let menuopening = "<div class='menu'> <ul>"
+    let menuclosing = "</ul> </div>"
+
+    divmenu.innerHTML = (menuopening + menuclosing);
 }
--- script/signuplogin.js
+++ script/signuplogin.js
@@ -1,5 +1,11 @@
 $(document).ready(function (){
-$('#donthave').on("click", function hide(){
-    $('.login').toggle("slow");
-})
-})
+    let box = $('.login');
+$('#donthave').click(function hidecontent(){
+    $(box).hide();
+
+    $(box).before("<div class='signup'> <div class='credencials'> <input type='email' placeholder='EMAIL'> <br> <input type='email' placeholder='CONFIRMAR EMAIL'> <br> <input type='password' placeholder='SENHA'> <br> <input type='password' placeholder='CONFIRMAR SENHA'> <div class='alternatives'> <a href='index.html'> <button id='signupac'>CADASTRAR-SE");
+    $(box).before("</button> </a> </div> </div> </div>");
+
+    
+});
+});
